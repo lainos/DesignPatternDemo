e17acc9333015995ad255b336a1211c6bf08d7e9
于大贱 <503319102@qq.com>
2017-03-10 14:30:47 +0800

    Initial commit

---
+++ app/src/main/java/httpdemo/yzx/com/httpdemo/MainActivity.java
@@ -0,0 +1,15 @@
+package httpdemo.yzx.com.httpdemo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import static httpdemo.yzx.com.httpdemo.R.*;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layout.activity_main);
+    }
+}
